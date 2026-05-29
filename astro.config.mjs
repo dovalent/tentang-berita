5989bfcd5cc5d6fd9864d58f2a0834d72402ca21
TENTANG Berita <admin@tentangberita.com>
2026-05-29 11:12:01 +0700

    Add sitemap for Google Search indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://tentang-berita.vercel.app',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'static',
   adapter: vercel(),
   image: {
